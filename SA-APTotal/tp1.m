1a42c8de50af67526edf139650c62ce61c9ea6dd
Samuel Lima <samuel_mbl@hotmail.com>
2019-05-10 23:02:41 -0300

    Created folders for each optimization algorithm

--- SA-APTotal/tp1.m
+++ SA-APTotal/tp1.m
@@ -2,7 +2,7 @@ clc;
 close all;
 clear all;
 
-clients = load('clientes.csv');
+clients = load('../clientes.csv');
 
 plot(clients(:,1), clients(:,2), 'b.');
 
---
+++ SA-Distance/tp1.m
@@ -0,0 +1,9 @@
+clc;
+close all;
+clear all;
+
+clients = load('../clientes.csv');
+
+plot(clients(:,1), clients(:,2), 'b.');
+
+
